10633d50130d44bc69bf4424f542a07f8b2bb2ca
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-01-09 21:40:16 -0500

    Enable CrossOrigin, implement SenseHAT

--- src/main/java/com/weather/weatherserver/API/WeatherAPI.java
+++ src/main/java/com/weather/weatherserver/API/WeatherAPI.java
@@ -1,16 +1,23 @@
 package com.weather.weatherserver.API;
 
 import com.weather.weatherserver.Models.Weather;
+import com.weather.weatherserver.RPI.SenseHat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/API")
 public class WeatherAPI {
+
     @GetMapping("/weather")
     public Weather getWeather(){
-        //Example information. Connect this to the library I created later.
-        return new Weather(25.0, 1005, 33.42);
+        //Uses the SenseHAT class to get current weather information, and return the new Weather object as JSON API.
+        double temp = SenseHat.getTemperatureAveraged();
+        double humidity = SenseHat.getHumidity();
+        double pressure = SenseHat.getPressureMbar();
+        return new Weather(temp, humidity, pressure);
     }
 }
--- src/main/java/com/weather/weatherserver/WeatherServerApplication.java
+++ src/main/java/com/weather/weatherserver/WeatherServerApplication.java
@@ -2,6 +2,9 @@ package com.weather.weatherserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class WeatherServerApplication {
@@ -9,5 +12,4 @@ public class WeatherServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeatherServerApplication.class, args);
     }
-
 }
