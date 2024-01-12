1b7dc4d365810bcf013d4f4a5dc5407022ad0eac
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-01-12 12:55:34 -0500

    Refactor package

--- src/main/java/com/chrisbarbati/weatherserver/API/WeatherAPI.java
+++ src/main/java/com/chrisbarbati/weatherserver/API/WeatherAPI.java
@@ -1,6 +1,6 @@
 package com.chrisbarbati.weatherserver.API;
 
-import com.chrisbarbati.weatherserver.RPI.SenseHat;
+import com.chrisbarbati.weatherserver.RPI.SenseHATI2C;
 import com.chrisbarbati.weatherserver.Models.Weather;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +15,9 @@ public class WeatherAPI {
     @GetMapping("/weather")
     public Weather getWeather(){
         //Uses the SenseHAT class to get current weather information, and return the new Weather object as JSON API.
-        double temp = SenseHat.getTemperatureAveraged();
-        double humidity = SenseHat.getHumidity();
-        double pressure = SenseHat.getPressureMbar();
+        double temp = SenseHATI2C.getTempAveraged();
+        double humidity = SenseHATI2C.getHumidity();
+        double pressure = SenseHATI2C.getPressureMbar();
         return new Weather(temp, humidity, pressure);
     }
 }
