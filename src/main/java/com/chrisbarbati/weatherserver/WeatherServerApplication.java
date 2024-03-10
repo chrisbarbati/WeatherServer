a2f27256acfb8a42a58af4c5209067b2f671c173
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2024-03-10 15:55:38 -0400

    Remove unused import

--- src/main/java/com/chrisbarbati/weatherserver/WeatherServerApplication.java
+++ src/main/java/com/chrisbarbati/weatherserver/WeatherServerApplication.java
@@ -2,7 +2,6 @@ package com.chrisbarbati.weatherserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
